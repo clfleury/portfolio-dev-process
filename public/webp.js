c408d425d9f1cd62b32e57e8f188bf32af36994c
Colette Fleury <cfleury@nesn.com>
2020-03-27 23:06:03 -0400

    update image type

---
+++ public/webp.js
@@ -0,0 +1,26 @@
+/*const imagemin = require("imagemin");
+const webp = require("imagemin-webp");
+
+imagemin(["sources/*.png"], "images", {
+  use: [
+    webp({
+      quality: 75
+    })
+  ]
+}).then(function() {
+  console.log("Images converted!");
+});*/
+
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+
+imagemin(['sources/*.{jpg,png}'], {
+  destination: __dirname + '/images/',
+  plugins: [
+    imageminWebp({
+      quality: 75
+    })
+  ]
+}).then(() => {
+  console.log('Images optimized');
+});
